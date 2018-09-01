0af275534de1c1e1a3bb97106ea94f5354117c52
alexandrefilimonov <alexflido@yahoo.com>
2018-08-31 21:58:22 -0700

    PHP1 Lesson4 Home Work

---
+++ php1lesson4.php
@@ -0,0 +1,23 @@
+﻿<?php
+echo '<hr />';
+echo 'PHP1 Lesson4:<br />';
+$n = 3;
+$s = "<ul>Chose your bouquet of flowers to purchase:<br>"; 
+// Open a known directory, and proceed to read its contents  
+
+$files = glob("img/*.jpg");
+foreach($files as $jpg){
+	$s = $s . "<li><a href='".$jpg."' target='_new'><img src='".$jpg."' width='75px'></a><br /></li><br>";
+}
+
+for($i=1;$i<=$n; $i++) {
+}
+$s = $s."</ul>";
+?>
+<html>
+<head></head>
+<body>
+	<?=$s?>
+</body>
+</html>
+
